eee55882bb73b0c960d99d530459cefa52ddd75f
Mapics <nadjarmatt@gmail.com>
2023-11-15 15:04:13 +0100

    litttle

--- server.js
+++ server.js
@@ -69,6 +69,46 @@ app.get("/articles/:id", async (req, res) => {
     }
 });
 
+app.get("/articles/user/:id", async (req, res) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const rows = await conn.query('SELECT * FROM articles WHERE user_id = ?', [req.params.id]);
+        res.status(200).json(rows);
+    } catch(err) {
+        res.status(404).json(err);
+    } finally {
+        if (conn) conn.release(); // Toujours libérer la connexion après usage
+    }
+});
+
+app.post("/articles", async (req, res) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+
+        const { title, content, author } = req.body;
+
+        const result = await conn.query(
+            'INSERT INTO articles (title, content, author) VALUES (?, ?, ?)',
+            [title, content, author]
+        );
+
+        // Récupère l'ID de l'article nouvellement créé
+        const insertedId = result.article_id;
+
+        // Récupère l'article créé en le sélectionnant par son ID
+        const newArticle = await conn.query('SELECT * FROM articles WHERE article_id = ?', [insertedId]);
+
+        res.status(201).json(newArticle[0]);  // Répond avec l'article nouvellement créé
+    } catch (err) {
+        res.status(500).json({ error: "Erreur lors de la création de l'article." });
+    } finally {
+        if (conn) conn.release(); // Toujours libérer la connexion après usage
+    }
+});
+
+
 
 app.listen(8000, () => {
     console.log("Serveur a l'ecoute");
